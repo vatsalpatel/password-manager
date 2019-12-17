ca341b3998ee5ebe42e8ea065853f5e3e6cd3685
N3onFrost <vasu20.06.2001@gmail.com>
2019-12-17 13:25:18 +0530

    Add VaultItem and Folder viewsets

--- securebits/api/views.py
+++ securebits/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import VaultItemSerializer, FolderSerializer
+from .models import VaultItem, Folder
+
 
 # Create your views here.
+class VaultItemViewSet(viewsets.ModelViewSet):
+    serializer_class = VaultItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return VaultItem.objects.filter(user=self.request.user)
+
+
+class FolderViewSet(viewsets.ModelViewSet):
+    serializer_class = FolderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Folder.objects.get(user=self.request.user)
