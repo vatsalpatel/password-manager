32e790bae7489908e2936f9d598d50276b7a9869
N3onFrost <vasu20.06.2001@gmail.com>
2019-12-17 13:27:13 +0530

    Register models with admin

--- securebits/api/admin.py
+++ securebits/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import VaultItem, Folder, VaultUser
+
 
 # Register your models here.
+admin.site.register(VaultUser)
+admin.site.register(VaultItem)
+admin.site.register(Folder)
