1f4c55c68dbf6cff436b6e5210fe51a76bf02c5f
N3onFrost <vasu20.06.2001@gmail.com>
2019-12-26 13:09:45 +0530

    Create Folders

--- securebits/frontend/src/components/Container.js
+++ securebits/frontend/src/components/Container.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core'
 import Folder from './Folder'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 function Container(props) {
     const folders = props.folders.map(folder => {
-        return <Folder folder={folder} />
+        return (
+            <div className="folder" style={{margin: 5}}>
+                <ExpansionPanel defaultExpanded style={{backgroundColor: "#36393e", color:"#fff"}}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon style={{color:"white"}} />}>
+                        <p>{folder.name}</p>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Folder folder={folder} />
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+        )
     })
     console.log(props)
     return (
-        <div className="ctn">
+        <>
             {folders}
-        </div>
+        </>
     )
 }
 
--- securebits/frontend/src/components/Folder.js
+++ securebits/frontend/src/components/Folder.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Vault from './Vault'
+import { Grid, Card, CardContent, CardActions } from '@material-ui/core'
 
 function Folder(props) {
     console.log(props.folder.id)
     const folderVaults = props.vaults.filter(vault => { return vault.folder === props.folder.id })
     const vaults = folderVaults.map(vault => {
-        return <Vault vault={vault} />
+        return (
+            <div className="vault" style={{padding: 10}}>
+                <Card>
+                    <CardContent>
+                        <Vault vault={vault} />
+                    </CardContent>
+                    <CardActions>
+
+                    </CardActions>
+                </Card>
+            </div>
+        )
     })
     console.log(props)
     return (
-        <div className="folder">
-            {vaults}
-        </div>
+        <>
+            <Grid container direction="row" justify="flex-start">
+                {vaults}
+            </Grid>
+        </>
     )
 }
 
