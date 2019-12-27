44f37905ef3708bca079e6ff3964d91fa2cb556a
N3onFrost <vasu20.06.2001@gmail.com>
2019-12-27 09:28:02 +0530

    Navbar for no user

--- securebits/frontend/src/components/Navbar.js
+++ securebits/frontend/src/components/Navbar.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { AppBar, Toolbar, Typography, Button, Grid, Link } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Button, Link} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-class Navbar extends React.Component {
-    render() {
-        return (
-            <>
-                
-            </>
-        )
-    }
+const useStyles = makeStyles({
+    toolbar: {
+        flexWrap: 1,
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+    },
+    link: {
+        marginLeft: 15,
+    },
+})
+
+function Navbar(props) {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static" color="default">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h6" color="primary" className={classes.toolbarTitle}>
+                    Secure Bits
+                </Typography>
+                <nav>
+                    <Link variant="button" color="textPrimary" href="#" className={classes.link}>Home</Link>
+                    <Link variant="button" color="textPrimary" href="#" className={classes.link}>About</Link>
+                    <Link variant="button" color="textPrimary" href="#" className={classes.link}>Contact</Link>
+                </nav>
+                {props.username === "" ?
+                    <>
+                        <Button variant="outlined" color="primary" className={classes.link}>Log In</Button>
+                        <Button variant="contained" color="primary" className={classes.link}>Sign Up</Button>
+                    </> :
+                    <>
+
+                    </>}
+            </Toolbar>
+        </AppBar>
+    )
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+    user: state.user,
+})
+
+export default connect(mapStateToProps, null)(Navbar);
