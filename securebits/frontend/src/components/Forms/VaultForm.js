e6230326bb91ac8783a8f6aaeb801bc38f69841f
n3onfrost <vasu20.06.2001@gmail.com>
2019-12-31 07:16:28 +0530

    Add password toggle to Form

--- securebits/frontend/src/components/Forms/VaultForm.js
+++ securebits/frontend/src/components/Forms/VaultForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Button, TextField, Dialog, DialogActions, DialogContent, MenuItem } from '@material-ui/core';
+import { Button, TextField, IconButton, Dialog, DialogActions, DialogContent, MenuItem, InputAdornment } from '@material-ui/core';
 import { withFormik } from 'formik';
 import { makeStyles } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { addVault } from '../../_actions/actions';
 
 const useStyles = makeStyles({
@@ -17,6 +19,7 @@ const useStyles = makeStyles({
 const Form = props => {
     const classes = useStyles();
     const [folder, setFolder] = useState(props.folders[0].id);
+    const [showPass, setShowPass] = useState(false);
     const {
         values,
         handleChange,
@@ -25,11 +28,14 @@ const Form = props => {
     } = props;
 
     const handleSubmit = () => {
-        submit({name: values.name ,username: values.username, password: values.password, folder:folder})
+        submit({ name: values.name, username: values.username, password: values.password, folder: folder })
         handleClose()
     }
     const handleSelect = event => {
         setFolder(event.target.value);
+    }
+    const togglePass = () => {
+        setShowPass(!showPass);
     }
 
     return (
@@ -42,7 +48,8 @@ const Form = props => {
                     name="username" value={values.username} onChange={handleChange}
                 />
                 <TextField variant="outlined" label="Password" fullWidth className={classes.text}
-                    type="password" name="password" value={values.password} onChange={handleChange}
+                    type={showPass ? "text" : "password"} name="password" value={values.password} onChange={handleChange}
+                    InputProps={{ endAdornment: <InputAdornment><IconButton onClick={togglePass}>{showPass ? <Visibility /> : <VisibilityOff />}</IconButton></InputAdornment> }}
                 />
                 <TextField variant="outlined" label="Folder" fullWidth className={classes.text}
                     select name="folder" value={folder} onChange={handleSelect}>
