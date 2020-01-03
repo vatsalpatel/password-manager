935bad35e335785c4a0592ba49b5c165afe40e4d
N3onFrost <vasu20.06.2001@gmail.com>
2020-01-03 10:26:28 +0530

    Refactor VaultForm

--- securebits/frontend/src/components/Forms/VaultForm.js
+++ securebits/frontend/src/components/Forms/VaultForm.js
@@ -19,73 +19,63 @@ const useStyles = makeStyles({
 const Form = props => {
     const classes = useStyles();
     const {
+        open,
+        onClose,
         values,
-        handleChange,
+        errors,
+        handleSubmit,
         handleClose,
-        submit,
+        handleChange,
     } = props;
-    const [folder, setFolder] = useState(values.folder || props.folders[0].id);
-    const [showPass, setShowPass] = useState(false);
 
-    const handleSubmit = () => {
-        submit({ id: values.id ,name: values.name, username: values.username, password: values.password, folder: folder })
-        handleClose()
-    }
-    const handleSelect = event => {
-        setFolder(event.target.value);
-    }
-    const togglePass = () => {
-        setShowPass(!showPass);
-    }
+    const [showPass, setShowPass] = useState(false)
+    const togglePass = () => setShowPass(!showPass)
 
     return (
-        <form>
-            <DialogContent className={classes.dialogContent}>
-                <TextField variant="outlined" label="Name" fullWidth className={classes.text}
-                    name="name" value={values.name} onChange={handleChange}
-                />
-                <TextField variant="outlined" label="Username" fullWidth className={classes.text}
-                    name="username" value={values.username} onChange={handleChange}
-                />
-                <TextField variant="outlined" label="Password" fullWidth className={classes.text}
-                    type={showPass ? "text" : "password"} name="password" value={values.password} onChange={handleChange}
-                    InputProps={{ endAdornment: <InputAdornment><IconButton onClick={togglePass}>{showPass ? <Visibility /> : <VisibilityOff />}</IconButton></InputAdornment> }}
-                />
-                <TextField variant="outlined" label="Folder" fullWidth className={classes.text}
-                    select name="folder" value={folder} onChange={handleSelect}>
-                    {props.folders.map(folder => (
-                        <MenuItem key={folder.id} value={folder.id}>{folder.name}</MenuItem>
-                    ))}
-                </TextField>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose} variant="outlined" color="secondary">Cancel</Button>
-                <Button onClick={handleSubmit} variant="contained" color="primary">Save</Button>
-            </DialogActions>
-        </form>
-    )
-}
-
-const FormikForm = withFormik({
-    mapPropsToValues: (props) => ({
-        id: props.vault.id,
-        name: props.vault.name,
-        username: props.vault.username,
-        password: props.vault.password,
-        folder: props.vault.folder,
-    })
-})(Form);
-
-const VaultForm = props => {
-    return (
-        <Dialog onClose={props.onClose} open={props.open} maxWidth="xs" fullWidth>
-            <FormikForm handleClose={props.onClose} folders={props.folders} submit={props.vault.id ? props.editVault : props.addVault} vault={props.vault} />
+        <Dialog onClose={onClose} open={open} maxWidth="xs" fullWidth>
+            <form>
+                <DialogContent className={classes.dialogContent}>
+                    <TextField variant="outlined" label="Name" fullWidth className={classes.text}
+                        name="name" value={values.name} onChange={handleChange}
+                    />
+                    <TextField variant="outlined" label="Username" fullWidth className={classes.text}
+                        name="username" value={values.username} onChange={handleChange}
+                    />
+                    <TextField variant="outlined" label="Password" fullWidth className={classes.text}
+                        type={showPass ? "text" : "password"} name="password" value={values.password} onChange={handleChange}
+                        InputProps={{ endAdornment: <InputAdornment><IconButton onClick={togglePass}>{showPass ? <Visibility /> : <VisibilityOff />}</IconButton></InputAdornment> }}
+                    />
+                    <TextField variant="outlined" label="Folder" fullWidth className={classes.text}
+                        select name="folder" value={values.folder} onChange={handleChange}>
+                        {props.folders.map(folder => (
+                            <MenuItem key={folder.id} value={folder.id}>{folder.name}</MenuItem>
+                        ))}
+                    </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} variant="outlined" color="secondary">Cancel</Button>
+                    <Button onClick={handleSubmit} variant="contained" color="primary">Save</Button>
+                </DialogActions>
+            </form>
         </Dialog>
     )
 }
 
+const VaultForm = withFormik({
+    mapPropsToValues: (props) => ({
+        name: props.vault.name,
+        username: props.vault.username,
+        password: props.vault.password,
+        folder: props.vault.folder || props.folders[0].id,
+    }),
+    enableReinitialize: true,
+    handleSubmit: (values, { props }) => {
+        console.log(values)
+    }
+})(Form)
+
 const mapStateToProps = (state, ownProps) => {
-    let vault = { id: 0, name: "", username: "", password: "", folder: "" };
+    let vault = { id: 0, name: "", username: "", password: "", folder: 0 };
     if (ownProps.vault) {
         vault = state.vaults.filter(vault => vault.id === ownProps.vault)[0]
     }
