f0731619add1494c553d9825f8ff3a37a8dab941
n3onfrost <vasu20.06.2001@gmail.com>
2020-01-21 07:59:26 +0530

    Add whitenoise

--- securebits/securebits/settings.py
+++ securebits/securebits/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'api',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'securebits.urls'
