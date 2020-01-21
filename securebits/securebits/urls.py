cb6c96a6c2b6f45168fc2ab82419e17cff4016e1
n3onfrost <vasu20.06.2001@gmail.com>
2020-01-21 07:40:58 +0530

    Setup app for heroku

--- securebits/securebits/urls.py
+++ securebits/securebits/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from rest_framework.routers import SimpleRouter
 from api.views import VaultViewSet, FolderViewSet
@@ -29,8 +30,5 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include(router.urls)),
     path('', TemplateView.as_view(template_name="index.html")),
-    path('vault/', TemplateView.as_view(template_name="index.html")),
-    path('folder/', TemplateView.as_view(template_name="index.html")),
-    path('settings/', TemplateView.as_view(template_name="index.html")),
-    path('about/', TemplateView.as_view(template_name="index.html")),
+    url(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
 ]
