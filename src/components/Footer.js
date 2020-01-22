a7dc3671bd41d33ac84c77dbac7ee504d09625db
N3onFrost <vasu20.06.2001@gmail.com>
2020-01-22 15:43:47 +0530

    Footer Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Grid, IconButton, Link, Tooltip } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, Link, IconButton, Dialog, DialogContent, DialogActions, Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
+import CodeIcon from '@material-ui/icons/Code';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EmailIcon from '@material-ui/icons/Email';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -15,21 +16,21 @@ const useStyles = makeStyles({
         flexDirection: "row",
     },
     left: {
-        flexDirection: "row",
         flexGrow: 1,
         marginLeft: "2em",
     },
     right: {
         marginRight: "2em",
     },
-    tooltip: {
-        fontSize: "1em",
-    }
 })
 
 function Footer(props) {
     const classes = useStyles()
     let history = useHistory()
+
+    const [dialog, setDialog] = useState(false)
+    const showDialog = () => setDialog(true)
+    const closeDialog = () => setDialog(false)
 
     const redirectAbout = () => history.push('/about/')
 
@@ -38,14 +39,23 @@ function Footer(props) {
             <Grid className={classes.footer} container>
                 <Grid className={classes.left}>
                     <span onClick={redirectAbout}>About Us</span>
-                    <Link color="inherit" href="https://www.github.com/n3onfrost"><IconButton color="inherit"><GitHubIcon /></IconButton></Link>
-                    <Link color="inherit" href="https://www.linkedin.com/"><IconButton color="inherit"><LinkedInIcon /></IconButton></Link>
-                    <Tooltip title="n3onfrost@gmail.com" className={classes.tooltip} arrow><IconButton color="inherit"><EmailIcon /></IconButton></Tooltip>
+                    <Link href="#" color="inherit"><IconButton color="inherit"><GitHubIcon /></IconButton></Link>
+                    <Link href="#" color="inherit"><IconButton color="inherit" ><LinkedInIcon /></IconButton></Link>
+                    <IconButton color="inherit" onClick={showDialog} ><EmailIcon /></IconButton>
                 </Grid>
                 <Grid className={classes.right}>
+                    <IconButton color="inherit"><CodeIcon /></IconButton>
                     <span>Developed by Vatsal Patel</span>
                 </Grid>
             </Grid>
+            <Dialog open={dialog} onClose={closeDialog} maxWidth="xs" fullWidth>
+                <DialogContent>
+                    <Typography variant="h6">My E-mail Address: n3onfrost@gmail.com</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" onClick={closeDialog}>Close</Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
