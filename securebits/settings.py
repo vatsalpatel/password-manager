c886269546b8e3428a55588ee2448c99244e7505
N3onFrost <vasu20.06.2001@gmail.com>
2020-01-23 15:21:10 +0530

    Remove database credentials

--- securebits/settings.py
+++ securebits/settings.py
@@ -104,9 +104,6 @@ DJOSER = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "securebits",
-        'USER': "securebitsuser",
-        'PASSWORD': "secretpass",
     }
 }
 
