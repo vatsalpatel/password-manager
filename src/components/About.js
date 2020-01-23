18358856dae7cce0e0f5c124f9ff36b18193e705
N3onFrost <vasu20.06.2001@gmail.com>
2020-01-23 12:08:03 +0530

    Start about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { indigo } from '@material-ui/core/colors';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles( theme => ({
     wrapper: {
         display: "flex",
-        justifyContent: "center",
+    },
+    title: {
+        color: indigo[900],
+        marginBottom: "0.5em",
+    },
+    main: {
+        alignSelf: "center",
     }
-})
+}))
 
 function About(props) {
     const classes = useStyles();
     return (
-        <Container maxWidth="md" className={classes.wrapper}>
-            
-        </Container>
+        <>
+            <Container className={classes.wrapper} maxWidth="md">
+                <Grid xs={12} className={classes.main}>
+                    <Typography variant="h4" className={classes.title}>What is Secure Bits?</Typography>
+                    <Typography>
+                        SecureBits is a password manager made with security in mind.
+                        Users have to remember only one master password which will unlock 
+                        all of their other passwords securely placed into vaults.
+                    </Typography>
+                </Grid>
+                <Grid xs={12}>
+                    <img src="/static/pm4.png" width="100%" />
+                </Grid>
+            </Container>
+        </>
     )
 }
 
