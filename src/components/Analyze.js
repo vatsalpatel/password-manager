4f90711269a1b75e7f19128cce11ce4b49cdbba8
N3onFrost <vasu20.06.2001@gmail.com>
2020-02-27 11:35:20 +0530

    Add password analsys page

--- src/components/Analyze.js
+++ src/components/Analyze.js
@@ -1,21 +1,98 @@
 import React from 'react';
-import {  } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Table, Container, TableBody, TableCell, TableHead, TableRow, Card, Grid } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
-import {  } from '../_actions/actions';
+import { } from '../_actions/actions';
 
 const useStyles = makeStyles({
-    
+    card: {
+        height: "10px",
+        width: "30%",
+    },
+    container: {
+        marginTop: "4em",
+    }
 })
+
+const StyledTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+}))(TableRow);
+
+
+const analyzeStrength = (pass) => {
+    let s = 0;
+    if (pass.length >= 8)
+        s += 1
+    if (pass.length > 15)
+        s += 2
+    if (pass.match(/[A-Z]/))
+        s += 1
+    if (pass.match(/[a-z]/))
+        s += 1
+    if (pass.match(/[0-9]/))
+        s += 1
+    if (pass.match(/[!@#$%&*-+=_:;,.?/]/))
+        s += 2
+    console.log(pass, s)
+    return s
+}
 
 function Analyze(props) {
     const classes = useStyles();
+    const { vaults } = props;
     return (
-        <></>
+        <>
+            <Container maxWidth="md" className={classes.container}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell>Username</StyledTableCell>
+                            <StyledTableCell>Password</StyledTableCell>
+                            <StyledTableCell>Strength</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            vaults.map(v => {
+                                return <StyledTableRow key={v.id}>
+                                    <StyledTableCell>{v.name}</StyledTableCell>
+                                    <StyledTableCell>{v.username}</StyledTableCell>
+                                    <StyledTableCell>
+                                        {'*'.repeat(v.password.length)}
+                                        {/* {v.password} */}
+                                    </StyledTableCell>
+                                    <StyledTableCell>
+                                            <Card className={classes.card}
+                                                style={{ backgroundColor: analyzeStrength(v.password) > 3 ? analyzeStrength(v.password) > 5 ? "limegreen" : "orange" : "orangered",
+                                                width: analyzeStrength(v.password) > 3 ? analyzeStrength(v.password) > 5 ? "90%" : "60%" : "30%" }} />
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                            })
+                        }
+                    </TableBody>
+                </Table>
+            </Container>
+        </>
     )
 }
 
 const mapStateToProps = state => ({
+    vaults: state.vaults,
 })
 
 export default connect(mapStateToProps, {})(Analyze);
