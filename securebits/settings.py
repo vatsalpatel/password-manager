fe3c370363c9f587c5b203da665935066372af84
vatsalpatel <vatsalpatel.me@gmail.com>
2022-12-09 19:41:15 +0530

    Change STATIC_ROOT

--- securebits/settings.py
+++ securebits/settings.py
@@ -149,12 +149,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build'),
-    os.path.join(BASE_DIR, 'build', 'static'),
+    os.path.join(BASE_DIR, 'build', 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
