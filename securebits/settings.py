304415d998a999eb765fb12486538f511e49e1f3
vatsalpatel <vatsalpatel.me@gmail.com>
2022-12-09 18:54:07 +0530

    Add STATIC_ROOT

--- securebits/settings.py
+++ securebits/settings.py
@@ -153,6 +153,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build', 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECURE_SSL_REDIRECT = True
