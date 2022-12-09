9f52f245364760d157270c648d3dc2564fef7a94
vatsalpatel <vatsalpatel.me@gmail.com>
2022-12-09 19:16:01 +0530

    Change STATIC_ROOT

--- securebits/settings.py
+++ securebits/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build', 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
