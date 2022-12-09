91407e71bf9e60ee01cfb560828b1cf180fd459d
vatsalpatel <vatsalpatel.me@gmail.com>
2022-12-09 20:18:36 +0530

    Add new URL to ALLOWED_HOSTS

--- securebits/settings.py
+++ securebits/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'securebits.herokuapp.com',
+    'plankton-app-ewh87.ondigitalocean.app',
+    
 ]
 
 
@@ -156,4 +158,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
